e949be218074b20341b1d11b097b161d4e152def
Mark Robson <roppa_uk@hotmail.com>
2015-09-21 17:02:32 -0700

    Refactored to use requestAnimationFrame.

--- public/js/views/WordifyView.js
+++ public/js/views/WordifyView.js
@@ -2,7 +2,7 @@ var WordifyView = Backbone.View.extend({
   
   className: 'wordify',
 
-  template: _.template('<div class="words"></div><div class="player"></div>'),
+  template: _.template('<div class="words"></div><div class="player"></div><button class="start">Start</button><button class="stop">Stop</button>'),
 
   events: {
     "click .start" : "play",
@@ -23,34 +23,9 @@ var WordifyView = Backbone.View.extend({
       return;
     }
 
-    //var chunks = wordify.wave(this.model.get('text'), [10,20,30,40]);
     this.player.state = true;
-    var element = this.$('.words');
-
-    this.player.anim = window.setInterval(function () {
-      if (this.player.count >= this.player.chunks.length) {
-        window.clearInterval(this.player.anim);
-        this.player.state = false;
-      }
-      this.$('.words').html(this.player.chunks[this.player.count]);
-      this.player.count++;
-    }.bind(this), this.player.wpm);
-
-    // this.player.anim = function (timestamp) {
-
-    // }:
-
-    // function step(timestamp) {
-    //   if (!start) start = timestamp;
-    //   var progress = timestamp - start;
-    //   element.style.left = Math.min(progress/10, 200) + "px";
-    //   if (progress < 2000) {
-    //     window.requestAnimationFrame(step);
-    //   }
-    // }
-
-    // window.requestAnimationFrame(step);
-
+    
+    window.requestAnimationFrame(this.player.anim);
 
   },
 
@@ -59,22 +34,42 @@ var WordifyView = Backbone.View.extend({
       return;
     }
     this.player.state = false;
-    //cancelAnimationFrame(this.player.anim);
-    window.clearInterval(this.player.anim);
+    cancelAnimationFrame(this.player.anim);
   },
 
   initialize: function() {
+
+    var start = Date.now();
+    var animationElement;
+
     this.render();
+
+    animationElement = this.$('.words');
+
+    this.player.anim = function (timestamp) {
+
+      var now = Date.now();
+
+      if (now - start > this.player.wpm) {
+        start = now;
+        animationElement.html(this.player.chunks[this.player.count]);
+        this.player.count++;
+      }
+      
+      if (this.player.state) {
+        window.requestAnimationFrame(this.player.anim);
+      }
+
+    }.bind(this);
+
   },
 
   render: function() {
-     
+    
+    //var chunks = wordify.wave(this.model.get('text'), [10,20,30,40]);
     this.player.chunks = wordify.chunk(this.model.get('text'), 2)
 
     this.$el.html(_.template(this.template()));
-
-    this.$el.append('<button class="start">Start</button>');
-    this.$el.append('<button class="stop">Stop</button>');
 
     return this;
 
