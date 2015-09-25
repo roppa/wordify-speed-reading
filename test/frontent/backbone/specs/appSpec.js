57002b641bf7633da8f1567b98b34b0fbd5d35ef
Mark Robson <roppa_uk@hotmail.com>
2015-09-25 10:05:14 -0700

    Added more tests

--- test/frontent/backbone/specs/appSpec.js
+++ test/frontent/backbone/specs/appSpec.js
@@ -1,3 +1,7 @@
 describe("Testing the app", function () {
 
+  it("should be an object", function () {
+    expect(app).to.be.an("object");
+  });
+
 });
--- test/frontent/backbone/specs/models/configModelSpec.js
+++ test/frontent/backbone/specs/models/configModelSpec.js
@@ -1,9 +1,23 @@
 describe("Testing the Config model", function () {
 
-  var article = new ConfigModel();
-
   it("should have default values", function () {
-    expect(article.attributes).to.have.all.keys("type", "wave", "wordSize", "wpm", "fontSize");
+
+    var config = new ConfigModel();
+    
+    expect(config).to.be.ok;
+  
+    expect(config.attributes).to.have.all.keys("type", "wave", "wordSize", "wpm", "fontSize");
+
+    expect(config.attributes.type).to.equal("chunk");
+
+    expect(config.attributes.wave).to.eql([20, 30, 40]);
+
+    expect(config.attributes.wordSize).to.equal(3);
+
+    expect(config.attributes.fontSize).to.equal(1.5);
+
+    expect(config.attributes.wpm).to.equal(300);
+
   });
 
 });
