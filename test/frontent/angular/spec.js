7b3204ce1d43b33fb25985f56719ab4b50e484ac
Mark Robson <roppa_uk@hotmail.com>
2015-10-14 20:06:54 +0100

    Adding protractor

---
+++ test/frontent/angular/spec.js
@@ -0,0 +1,11 @@
+describe('Wordify Angular App', function() {
+
+  beforeEach( function(){
+    browser.driver.get('http://localhost:3000?engine=angular');
+  });
+
+  it('user can navigate to the angular app', function(){
+      expect( browser.driver.getCurrentUrl() ).toContain('angular');
+  });
+
+});
