a719c28f32ca88f25d99a848988cb14666e5111c
Mark Robson <roppa_uk@hotmail.com>
2015-10-14 10:32:22 +0100

    Sorting out tests

--- test/server/environmentSpec.js
+++ test/server/environmentSpec.js
@@ -1,24 +1,16 @@
 var assert = require('assert');
 var request = require('supertest');
 
-var app = require('../app.js');
+var app = require('../../app.js');
 
 describe('app', function() {
 
-  xit('should have a PORT environment variable', function () {
+  it('should have a PORT environment variable', function () {
     assert(process.env.PORT !== undefined);
   });
   
   xit('should have a DB_HOST environment variable', function () {
     assert(process.env.DB_HOST !== undefined);
-  });
-
-  xit('should have a DB_USER environment variable', function () {
-    assert(process.env.DB_USER !== undefined);
-  });
-
-  xit('should have a DB_PASS environment variable', function () {
-    assert(process.env.DB_PASS !== undefined);
   });
 
 });
